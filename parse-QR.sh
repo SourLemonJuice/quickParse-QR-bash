c898a1f9d9b8ee62c1287dca5671af215e6812a9
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2024-01-07 20:43:01 +0800

    让终端输出更多的信息
    终端也不是主要输出有用信息的地方，就是拿来debug为什么还要安静输出

--- parse-QR.sh
+++ parse-QR.sh
@@ -13,22 +13,28 @@ TempFile_Path="/tmp/quick-QR-bash-TEMP"
 # 发通知时的应用名称
 App_Name="quick-QR-bash"
 
-# 执行前删除可能的临时文件
-rm -f "$TempFile_Path"
+# 执行前删除可能的临时文件，因为要检测是否截图了
+rm -vf "$TempFile_Path"
 
 # 启动截图，保存至临时文件夹 [--后台 --无通知 --自定义输出路径]
 spectacle "$Capture_Mode" --background --nonotify --output "$TempFile_Path"
 # 如果没有截图则退出(道理我都懂，但为什么没截图退出码还是 0 为什么啊啊啊w)
 [[ -f "$TempFile_Path" ]] || exit 1
 
-# 解析原始信息到 $Parsed_Code [--安静输出 --输出原始信息]
-Parsed_Code=$(zbarimg --quiet --raw "$TempFile_Path")
+# 解析原始信息到 $Parsed_Code [--输出原始信息]
+Parsed_Code=$(zbarimg --raw "$TempFile_Path")
 # 如果没检测到信息则退出
 if [ $? != 0 ]; then
     # 发信息提醒
     notify-send --app-name="$App_Name" "There's no QRcode in image"
+    # 同时在终端输出
+    echo "There's no QRcode in image"
+    # 清理临时文件，并退出
+    rm -vf "$TempFile_Path"
     exit 1
 fi
+# 终端输出解析后的信息
+echo "$Parsed_Code"
 
 # 发送带有复制按钮的通知 [--应用名称:显示在顶栏上的信息 --控件:返回信息=显示名称 --通知信息]
 Notify_Action=$(notify-send --app-name="$App_Name" --action Copy=Copy "$Parsed_Code")
@@ -50,7 +56,6 @@ Copy)
 ;;
 esac
 
-# 清理临时文件
-rm -f "$TempFile_Path"
-
+# 清理临时文件，并退出
+rm -vf "$TempFile_Path"
 exit 0
